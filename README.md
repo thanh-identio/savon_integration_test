079e5bce190535cef1197ec40e49f4af9bc105d6
Thanh Bui <thanh@identio.fi>
2023-09-18 12:21:50 +0300

    update README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# HOW TO START
+1.  ```source env/bin/activate```
+2. ```pip install -r requirement.txt```
+3. ```python3 manage.py runserver 0.0.0.0:8000```
+
+P/s: check if `ALLOWEDHOST` in `savon_text/setting.py` has your machine's IP
--- savon_test/settings.py
+++ savon_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&m9@f2hn)c2nnzw-u5i=lzmx91^93#%h*s=5821qj-8w)d8xb0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
